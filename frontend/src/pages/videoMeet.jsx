5878c931475a995af8a8bfc2b233ecd799b71516
Rahul Kamat <rahulkamatsvrk@gmail.com>
2026-05-01 03:25:15 +0530

    Deploying frontend

--- frontend/src/pages/videoMeet.jsx
+++ frontend/src/pages/videoMeet.jsx
@@ -12,7 +12,7 @@ import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 
-const server_url = "http://localhost:8000";
+const server_url = server;
 
 var connections = {};
 
